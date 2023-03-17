e73e7ed653979439a55548c0ac03b94a4d2170cf
skg1998 <sahilkg1998@gmail.com>
2023-03-17 20:46:12 +0530

    work on kubernetes

--- backend/EkartGateway/src/main/java/com/infy/ekart/gateway/ekartgateway/controller/FallbackController.java
+++ backend/EkartGateway/src/main/java/com/infy/ekart/gateway/ekartgateway/controller/FallbackController.java
@@ -2,9 +2,11 @@ package com.infy.ekart.gateway.ekartgateway.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin("*")
 @RestController
 public class FallbackController {
 

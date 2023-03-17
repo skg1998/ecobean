5ab196ba9effe8f310f9480d0cd2e1f7db72a188
nk65111 <nk6511165@gmail.com>
2023-03-17 22:11:52 +0530

    add pagination

--- backend/ProductMS/src/main/java/com/infy/ekart/product/repository/ProductRepository.java
+++ backend/ProductMS/src/main/java/com/infy/ekart/product/repository/ProductRepository.java
@@ -1,10 +1,10 @@
 package com.infy.ekart.product.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.infy.ekart.product.entity.Product;
 
 //extends it with required Interface
-public interface ProductRepository extends CrudRepository<Product, Integer> {
+public interface ProductRepository extends JpaRepository<Product, Integer>  {
 	
 }
